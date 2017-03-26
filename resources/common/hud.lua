469151721e59e2add9064b30c36faa19a229ebbe
Francesco Napoletano <napolux@gmail.com>
2017-03-26 18:41:37 +0200

    HUD localization

--- resources/common/hud.lua
+++ resources/common/hud.lua
@@ -1,23 +1,25 @@
 -- hud class
-Class = require "libraries.hump.class"
+Class  = require "libraries.hump.class"
+locale = require "libraries.locale.locale"
 
 Hud = Class { 
     
-    init = function(self)
+    init = function(self, ship)
         self.score = "00000000"
         self.highScore = "00010000"
+        self.lives = ship.lives
         self.screenWidth  = love.graphics.getWidth()
         self.screenHeight = love.graphics.getHeight()
     end; 
 
-    update = function(self, dt)
-
+    update = function(self, ship)
+        self.lives = ship.lives
     end;
 
     draw = function(self)
         love.graphics.setColor(255, 255, 255, 255)
-        love.graphics.printf("SCORE\n" .. self.score, 20, 20, self.screenWidth - 20, "left")
-        love.graphics.printf("HIGH SCORE\n" .. self.highScore, 0, 20, self.screenWidth - 20, "right")
+        love.graphics.printf(locale.getLocalized("hud.score") .. "\n" .. self.score, 20, 20, self.screenWidth - 20, "left")
+        love.graphics.printf(locale.getLocalized("hud.hiscore").. "\n" .. self.highScore, 0, 20, self.screenWidth - 20, "right")
     end;
 }
 
--- states/game.lua
+++ states/game.lua
@@ -18,7 +18,7 @@ local hud = require "resources.common.hud"
 function game:init()
     bg.init(bg)
     ship.init(ship)
-    hud.init(hud)
+    hud.init(hud, ship)
 end
 
 function game:enter(previous, endData)
@@ -31,7 +31,7 @@ function game:update(dt)
     Timer.update(dt)
     bg.update(bg, dt)
     ship.update(ship, dt)
-    hud.update(hud)
+    hud.update(hud, ship)
 end
 
 function game:draw()
